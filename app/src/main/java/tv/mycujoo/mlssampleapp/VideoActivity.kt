6bad18d629de7ace3f9ec49af1ca597ba4880286
shervinox <shervin@mycujoo.tv>
2020-05-12 12:46:54 +0200

    Add VideoActivity

---
+++ app/src/main/java/tv/mycujoo/mlssampleapp/VideoActivity.kt
@@ -0,0 +1,12 @@
+package tv.mycujoo.mlssampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VideoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video)
+    }
+}
