93f93e01cfd8a81baa1d9878b82b53a58406e986
shervinox <shervin@mycujoo.tv>
2020-05-12 12:56:33 +0200

    Add MLS as module

--- app/src/main/java/tv/mycujoo/mlssampleapp/VideoActivity.kt
+++ app/src/main/java/tv/mycujoo/mlssampleapp/VideoActivity.kt
@@ -1,7 +1,7 @@
 package tv.mycujoo.mlssampleapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class VideoActivity : AppCompatActivity() {
 
