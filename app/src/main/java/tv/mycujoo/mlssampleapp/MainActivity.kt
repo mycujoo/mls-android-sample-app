1f1bfb729c641d9a7950de539a5710ed47823459
shervinox <shervin@mycujoo.tv>
2020-05-14 14:40:24 +0200

    Replace dependency from local to Github-Packages

--- app/src/main/java/tv/mycujoo/mlssampleapp/MainActivity.kt
+++ app/src/main/java/tv/mycujoo/mlssampleapp/MainActivity.kt
@@ -1,12 +1,15 @@
 package tv.mycujoo.mlssampleapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, VideoActivity::class.java))
     }
 }
--- app/src/main/java/tv/mycujoo/mlssampleapp/VideoActivity.kt
+++ app/src/main/java/tv/mycujoo/mlssampleapp/VideoActivity.kt
@@ -1,12 +1,57 @@
 package tv.mycujoo.mlssampleapp
 
+
+import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_video.*
+import tv.mycujoo.mls.api.MyCujooLiveService
+import tv.mycujoo.mls.model.ConfigParams
 
 class VideoActivity : AppCompatActivity() {
+
+    lateinit var myCujooLiveService: MyCujooLiveService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video)
+
+        myCujooLiveService = MyCujooLiveService.Builder()
+            .withContext(this)
+            .defaultPlayerController(true)
+            .build()
+
+        myCujooLiveService.loadVideo(Uri.parse("https://playlists.mycujoo.football/eu/ck8u05tfu1u090hew2kgobnud/master.m3u8"))
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        myCujooLiveService.onStart(playerViewWrapper)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        myCujooLiveService.onResume(playerViewWrapper)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        myCujooLiveService.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        myCujooLiveService.onStop()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        myCujooLiveService.onConfigurationChanged(
+            ConfigParams(newConfig, hasPortraitActionBar = true, hasLandscapeActionBar = false),
+            window.decorView,
+            supportActionBar
+        )
     }
 }
