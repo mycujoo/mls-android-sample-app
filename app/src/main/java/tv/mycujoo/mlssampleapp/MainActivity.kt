098be2d1e8f2e1c360ca11d2a3990395382f1914
shervinox <shervin@mycujoo.tv>
2020-12-02 17:12:55 +0100

    Add TV sample to 'tv' branch

--- app/src/main/java/tv/mycujoo/mlssampleapp/MainActivity.kt
+++ app/src/main/java/tv/mycujoo/mlssampleapp/MainActivity.kt
@@ -1,33 +1,12 @@
 package tv.mycujoo.mlssampleapp
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.fragment.app.FragmentActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        withEventIdButton.setOnClickListener {
-            startActivity(
-                Intent(
-                    this,
-                    VideoActivity::class.java
-                )
-            )
-        }
-        withEventListButton.setOnClickListener {
-            startActivity(
-                Intent(
-                    this,
-                    VideoActivityWithEventList::class.java
-                )
-            )
-        }
-
-
     }
 }
